d5f807e9ada2a51a60b4b9c87a92a5507a7829ac
Ryan Graham <ryan@strongloop.com>
2014-09-30 17:08:53 -0700

    Ensure repos are saved on import

--- db/strongloop-seeds.rb
+++ db/strongloop-seeds.rb
@@ -48,5 +48,6 @@ repos.map { |repo|
                          repo_name: repo.name)
                   .first_or_create(AGREEMENT_TEMPLATE)
 }.each do |agreement|
-  puts "#{agreement.user_name}/#{agreement.repo_name}"
+  result = agreement.save
+  puts "#{agreement.user_name}/#{agreement.repo_name} => #{result}"
 end.tap { |all| p "Total: #{all.count}" }
